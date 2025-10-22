0b2d0d2f29fb5d40cbf8bd50c43b116d05b7e60e
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2025-10-22 08:58:12 +0100

    Wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+           'voters/store',
+           'voters/getPollingUnitsByWard',
+           'voters/getWardsByLga',
+           'voters/getAgeRanges',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
